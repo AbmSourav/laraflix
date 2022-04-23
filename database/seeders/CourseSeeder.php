89ce54ebdb94b19ffaa0aa49d2d49807ccc618aa
AbmSourav <keramotul.islam@gmail.com>
2022-04-23 14:55:39 +0600

    course,video table. seeders added

---
+++ database/seeders/CourseSeeder.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class CourseSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('courses')->insert([
+            [
+                'id' => 1,
+                'title' => "Laravel Course",
+                'description' => "It's a Laravel course.",
+                'created_at' => date('Y-m-d h:i:s'),
+                'updated_at' => date('Y-m-d h:i:s')
+            ],
+            [
+                'id' => 2,
+                'title' => "Laravel Advanced Course",
+                'description' => "It's a course for Laravel advanced learners.",
+                'created_at' => date('Y-m-d h:i:s'),
+                'updated_at' => date('Y-m-d h:i:s')
+            ]
+        ]);
+    }
+}
---
+++ database/seeders/VideoSeeder.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class VideoSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('videos')->insert([
+            [
+                'id' => 1,
+                'name' => "wp-plugin-cli.mp4",
+                'course_id' => 1,
+                'created_at' => date('Y-m-d h:i:s'),
+                'updated_at' => date('Y-m-d h:i:s')
+            ],
+            [
+                'id' => 2,
+                'name' => "talash.mp4",
+                'course_id' => 2,
+                'created_at' => date('Y-m-d h:i:s'),
+                'updated_at' => date('Y-m-d h:i:s')
+            ]
+        ]);
+    }
+}
